824a5dad62a7701a65f3b6a7649a7c27d0ddd92e
Martin Penckert <martin.penckert@cevt.se>
2022-12-01 22:53:22 +0100

    Adding specular lighting - so nice :)

--- lighting/cube_fs.glsl
+++ lighting/cube_fs.glsl
@@ -7,6 +7,7 @@ out vec4 FragColor;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main() {
     // when calculating lightning, magnitude or position of vectors don't 
@@ -19,10 +20,25 @@ void main() {
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
+    // calculate specular lighting
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    //                        ^ reflect needs a vector that points from the 
+    //                          light source to the fragment 
+
+    // specular component
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    //                                                   ^ the value of 
+    //                                      "shininess" as a power of 2;
+    //                                      the higher, the laser
+
+    vec3 specular = specularStrength * spec * lightColor;
+
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec3 result = (ambient + diffuse) * objectColor;
+    vec3 result = (ambient + diffuse + specular) * objectColor;
 
     FragColor = vec4(result, 1.0);
 }
