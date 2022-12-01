18d908a245e81c59de3c24481dad59bba65b3b04
Martin Penckert <martin.penckert@cevt.se>
2022-12-01 19:35:58 +0100

    Add light source

--- lighting/main.cpp
+++ lighting/main.cpp
@@ -15,7 +15,11 @@ const unsigned int SCR_WIDTH = 1200;
 const unsigned int SCR_HEIGHT = 900;
 const char *TITLE = "Colors & Lighting :: OpenGL";
 
+// camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+// lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
@@ -47,7 +51,9 @@ int main()
     glEnable(GL_DEPTH_TEST);
 
     // ..:: SHADERS ::..
-    Shader shader("vertex_shader.glsl", "fragment_shader.glsl");
+    Shader lightingShader("vertex_shader.glsl", "fragment_shader.glsl");
+    Shader lightSourceShader("vertex_shader.glsl",
+                             "light_source_fragment_shader.glsl");
 
     // ..:: VERTICES ::..
     float vertices[] = {
@@ -64,15 +70,26 @@ int main()
         0.5f,  0.5f,  -0.5f, 0.5f,  0.5f,  -0.5f, 0.5f,  -0.5f,
     };
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
+    unsigned int VBO, cubeVAO;
+    glGenVertexArrays(1, &cubeVAO);
     glGenBuffers(1, &VBO);
 
-    glBindVertexArray(VAO);
+    glBindVertexArray(cubeVAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+
+    // only needs binding, no need to fill it again.
+    // TODO: understand this magic
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
                           (void *)0);
     glEnableVertexAttribArray(0);
@@ -85,29 +102,46 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        shader.use();
-        shader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-        shader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+        // activate shader and set uniform and drawing objects
+        lightingShader.use();
+        lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 
+        // view & projection transformation
         glm::mat4 projection = glm::perspective(
             glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT,
             0.1f, 100.0f);
-        shader.setMat4("projection", projection);
+        lightingShader.setMat4("projection", projection);
 
         glm::mat4 view = camera.GetViewMatrix();
-        shader.setMat4("view", view);
+        lightingShader.setMat4("view", view);
 
+        // world transformation
         glm::mat4 model = glm::mat4(1.0f);
-        shader.setMat4("model", model);
+        lightingShader.setMat4("model", model);
 
-        glBindVertexArray(VAO);
+        // render the cube
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // draw lamp
+        lightSourceShader.use();
+        lightSourceShader.setMat4("projection", projection);
+        lightSourceShader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f));
+        lightSourceShader.setMat4("model", model);
+
+        glBindVertexArray(lightVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
     // ..:: CLEANUP ::..
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightVAO);
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
